111b0693b0ce0bda2e181528202980162597af56
AmanGupta0510 <amanrajgupta7677@gmail.com>
2025-12-27 01:51:24 +0530

    Solution

--- HttpServer/HttpServer.java
+++ HttpServer/HttpServer.java
@@ -1,5 +1,6 @@
 package HttpServer;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
@@ -12,18 +13,19 @@ public class HttpServer{
     private static final String HTTP_NEW_LINE_SEPERATOR = "\r\n";
     private static final String HTTP_HEAD_BODY_SEPERATOR = HTTP_NEW_LINE_SEPERATOR+HTTP_NEW_LINE_SEPERATOR;
     private static final int DEFAULT_PACKET_SIZE = 10_000;
+    private static final String CONTENT_LENGTH_HEADER = "content-length";
+    private static final int HTTP_HEAD_BODY_SEPERATOR_BYTES = HTTP_HEAD_BODY_SEPERATOR.getBytes(StandardCharsets.US_ASCII).length;
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(8080) ;
         System.out.println("Listening on http://127.0.0.1:8080/");
 
         while (true) {
             Socket connection = serverSocket.accept();
-            var request = readRequest(connection);
-            // if(request.isEmpty()){
-            //     continue;
-            // }
-            // System.out.println("We have a request"+request);
-            printRequest(request);
+            var requestOpt = readRequest(connection);
+            if(requestOpt.isEmpty())continue;
+            
+           printRequest(requestOpt);
+
 
             try (var os = connection.getOutputStream()) {
 
@@ -42,57 +44,125 @@ public class HttpServer{
         }
     }
 
-    private static HttpReq readRequest(Socket connection ) throws Exception{
+    private static void printRequest(Optional<HttpReq> requestOpt) throws Exception {
+         HttpReq req = requestOpt.get();// unwrap
+        if (requestOpt.isPresent()) {
+             
+            System.out.println("Methods: "+ req.method);
+            System.out.println("Url: "+ req.url);
+            System.out.println("Headers:");
+            req.header.forEach((k,v)->{
+            System.out.println("%s - %s".formatted(k,v));
+        });
+        }
+        if(req.body.length > 0){
+           System.out.println(new String(req.body,StandardCharsets.UTF_8));
+        }
+        else{
+            System.out.println("Body is Empty");
+        }
+       
+    }
+
+    private static Optional<HttpReq> readRequest(Socket connection ) throws Exception{
         var stream = connection.getInputStream();
+        var rawRequestHead = readRawRequestHead(stream);
+        if(rawRequestHead.length == 0){
+            return Optional.empty();
+        }
+        var requestHead = new String(rawRequestHead,StandardCharsets.US_ASCII); 
+        var lines = requestHead.split(HTTP_NEW_LINE_SEPERATOR);
+
+       
+        var line = lines[0];
+        var methodUrl = line.split(" ");
+        var method = methodUrl[0];
+        var url = methodUrl[1];
+        var headers = readHeader(lines); 
+        var bodyLength = getExpectedBodyLength(headers);
+        byte[] body;
+        if(bodyLength>0){
+            var bodyStartIndex = requestHead.indexOf(HTTP_HEAD_BODY_SEPERATOR);
+            if(bodyStartIndex > 0){
+                var readBody = Arrays.copyOfRange(rawRequestHead,bodyStartIndex+HTTP_HEAD_BODY_SEPERATOR_BYTES,rawRequestHead.length);
+                body = readBody(stream,readBody,bodyLength);
+
+
+            }
+            else{
+                body = new byte[0];
+            }
+        }    
+        else{
+            body = new byte[0];
+        }
+       
+        return Optional.of(new HttpReq(method,url,headers,body));
+    }
+
+    private static int getExpectedBodyLength(Map<String,List<String>> headers){
+        try{
+            return Integer.parseInt(headers.getOrDefault(CONTENT_LENGTH_HEADER,List.of("0")).get(0));
+        }catch(Exception ignored){
+            return 0;
+        }
+
+    }
+     
+    private static byte[] readRawRequestHead(InputStream stream) throws Exception{
+          
+       
         var toRead = stream.available();
         if(toRead == 0){
             toRead = DEFAULT_PACKET_SIZE;
         }
         var buffer = new byte[toRead];
         var read = stream.read(buffer);
-        // if(read<=0){
-        //     return Optional.empty();
-        // }
-
-        var r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            var line = r.readLine();
-            var methodUrl = line.split(" ");
-            var method = methodUrl[0];
-            var url = methodUrl[1];
-            // System.out.println("Request Line = "+line); 
-            var headers = readHeader(r); 
-            // var body = readBody(connection.getInputStream());  
-            
-        return new HttpReq(method,url,headers,null);
+        if(read<=0){
+            return new byte[0];
+        }
+        return read == toRead?buffer:Arrays.copyOf(buffer,read);
     }
-     
-    
-    private static Map<String,List<String>> readHeader(BufferedReader reader) throws Exception{
-        var line = reader.readLine();
+    private static Map<String,List<String>> readHeader(String[] lines) {
+      
 
         var headers = new HashMap<String,List<String>>();
-        while(line!=null && !line.isEmpty()){
+        for(int i = 1;i<lines.length;i++){
+            var line = lines[i];
+            if(line.isEmpty()){
+                break;
+            }
             var keyValue = line.split(":",2);
-            var key = keyValue[0];
-            var value = keyValue[1];
+            var key = keyValue[0].strip();
+            var value = keyValue[1].strip();
             headers.computeIfAbsent(key,k ->new ArrayList<>() ).add(value);
-            line = reader.readLine();
         }
+        
         return headers;
 
     }
     
-    // private static byte[] readBody(InputStream stream){
-    //    return new byte[];
-    // }
-    private static void printRequest(HttpReq req){
-        System.out.println("Methods: "+ req.method);
-        System.out.println("Url: "+ req.url);
-        System.out.println("Headers:");
-        req.header.forEach((k,v)->{
-            System.out.println("%s - %s".formatted(k,v));
-        });
+    private static byte[] readBody(InputStream stream,byte[] readBody,int expectedBodyLength )throws Exception{
+       if(readBody.length == expectedBodyLength){
+        return readBody;
+       } 
+       var result = new ByteArrayOutputStream(expectedBodyLength);
+       result.write(readBody);
+       var readBytes = readBody.length;
+       var buffer = new byte[DEFAULT_PACKET_SIZE];  
+       while(readBytes < expectedBodyLength){
+          var read = stream.read(buffer);
+          if(read > 0){
+            result.write(buffer,0,read);
+            readBytes+=read;
+          }else{
+            break;
+          }
+        
+       }
+       return result.toByteArray();
     }
+   
     private  record HttpReq(String method,String url,Map<String,List<String>> header,byte[] body){
 
     }
